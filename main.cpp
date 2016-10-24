f4fa3c3743d236a3d3b000079e3da96c268db069
Yimin <zhongym8903@gmail.com>
2016-10-24 02:08:53 -0500

    placeholder
    need to debug from here, problematic parameter radius.
    
    linear convergence observed only.

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main() {
     /*
      * allocating source, target locations and weights
      */
-    vector<point> coarseSource;
-    vector<point> coarseTarget;
-    vector<point> coarseTriangle;
-    vector<double> coarseWeight;
+    vector<point> coarseSource, fineSource;
+    vector<point> coarseTarget, fineTarget;
+    vector<point> coarseTriangle, fineTriangle;
+    vector<double> coarseWeight, fineWeight;
 
     /*
      * cube radius and sphere radius
@@ -32,7 +32,7 @@ int main() {
     /*
      * slice x slice grid on each face.
      */
-    int slice = 16;
+    int slice = 8;
 
     cubeProjection(coarseSource, coarseWeight, coarseTriangle, cRadius, sRadius, slice);
     coarseTarget = coarseSource;
@@ -47,11 +47,26 @@ int main() {
         coarseNormalZ[i] = coarseSource[i].z / sRadius;
     }
 
-    int np = 6;
+
+    cubeProjection(fineSource, fineWeight, fineTriangle, cRadius, sRadius, 2 * slice);
+    fineTarget = fineSource;
+
+    vector<double> fineNormalX(fineSource.size());
+    vector<double> fineNormalY(fineSource.size());
+    vector<double> fineNormalZ(fineSource.size());
+
+    for (int i = 0; i < fineSource.size(); ++i) {
+        fineNormalX[i] = fineSource[i].x / sRadius;
+        fineNormalY[i] = fineSource[i].y / sRadius;
+        fineNormalZ[i] = fineSource[i].z / sRadius;
+    }
+
+
+    int np = 3;
     int maxPoint = 160;
     int maxLevel = 10;
 
-    double k = 0.03;
+    double k = 1.0;
     double dE = 80.0;
     double dI = 2.0;
 
@@ -91,19 +106,19 @@ int main() {
     auto eval_pGkx = [&](point &a, point &b) {
         double d = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z);
         double r = sqrt(d);
-        return -(a.x - b.x) * (k * r + 1) / 4 / M_PI / r / d;
+        return -(a.x - b.x) * exp(-k * r) * (k * r + 1) / 4 / M_PI / r / d;
     };
 
     auto eval_pGky = [&](point &a, point &b) {
         double d = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z);
         double r = sqrt(d);
-        return -(a.y - b.y) * (k * r + 1) / 4 / M_PI / r / d;
+        return -(a.y - b.y) * exp(-k * r) * (k * r + 1) / 4 / M_PI / r / d;
     };
 
     auto eval_pGkz = [&](point &a, point &b) {
         double d = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z);
         double r = sqrt(d);
-        return -(a.z - b.z) * (k * r + 1) / 4 / M_PI / r / d;
+        return -(a.z - b.z) * exp(-k * r) * (k * r + 1) / 4 / M_PI / r / d;
     };
 
 
@@ -142,7 +157,7 @@ int main() {
             if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGky);
             else return eval_pGky(a, b);
         };
-        pG0z.eval = [&](point &a, point &b) {
+        pGkz.eval = [&](point &a, point &b) {
             if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGkz);
             else return eval_pGkz(a, b);
         };
@@ -189,10 +204,15 @@ int main() {
 
 
     int N = (int) coarseSource.size();
-
+    int M = (int) fineSource.size();
     auto coarseMap = [&](VectorXd &phi) {
         return Mapping(coarseSource, coarseTarget, coarseTriangle, coarseWeight, coarseNormalX, coarseNormalY,
                        coarseNormalZ, phi);
+    };
+
+    auto fineMap = [&](VectorXd &phi) {
+        return Mapping(fineSource, fineTarget, fineTriangle, fineWeight, fineNormalX, fineNormalY,
+                       fineNormalZ, phi);
     };
 
     VectorXd input(2 * N);
@@ -203,8 +223,8 @@ int main() {
         input(i + N) = -1.0 / dI / 4.0 / M_PI / sRadius / sRadius;
     }
 
-    //start = input; //
-    start.setZero();
+    start = input;
+//    start.setZero();
 
     VectorXd output(2 * N);
     output.setZero();
@@ -212,100 +232,38 @@ int main() {
         output(i) = 1.0 / dI / 4.0 / M_PI / sRadius;
     }
 
-    gmres(coarseMap, output, start, 20, 40, 1e-7);
+    gmres(coarseMap, output, start, 6, 40, 1e-7);
+
+
+    VectorXd input_(2 * M);
+    VectorXd start_(2 * M);
+
+    for (int i = 0; i < M; ++i) {
+        input_(i) = 1.0 / dE / 4.0 / M_PI / sRadius / (1 + k * sRadius);
+        input_(i + M) = -1.0 / dI / 4.0 / M_PI / sRadius / sRadius;
+    }
+
+//    start_.setZero();
+    start_ = input_;
+    VectorXd output_(2 * M);
+    output_.setZero();
+    for (int i = 0; i < M; ++i) {
+        output_(i) = 1.0 / dI / 4.0 / M_PI / sRadius;
+    }
+
+    gmres(fineMap, output_, start_, 6, 40, 1e-7);
+
+
+    vector<int> proj;
+    projection(proj, coarseSource, fineSource);
+    VectorXd ret(2 * N);
+    for (int i = 0; i < N; ++i) {
+        ret(i) = start_(proj[i]);
+        ret(i + N) = start_(M + proj[i]);
+    }
 
     std::cout << "coarse error :" << (start - input).norm() / input.norm() << std::endl;
-
-    auto rxn = [&](vector<point> &source, vector<point> &target, vector<point> triangle, vector<double> &weight,
-                   vector<double> &normalX, vector<double> &normalY, vector<double> &normalZ, VectorXd &phi) {
-        int N = (int) source.size();
-        int M = (int) target.size();
-
-        assert(phi.rows() == 2 * N);
-
-        kernel G0, Gk, pG0x, pG0y, pG0z, pGkx, pGky, pGkz;
-        G0.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_G0);
-            else return eval_G0(a, b);
-        };
-        Gk.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_Gk);
-            else return eval_Gk(a, b);
-        };
-        pG0x.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0x);
-            else return eval_pG0x(a, b);
-        };
-        pG0y.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0y);
-            else return eval_pG0y(a, b);
-        };
-        pG0z.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0z);
-            else return eval_pG0z(a, b);
-        };
-        pGkx.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGkx);
-            else return eval_pGkx(a, b);
-        };
-        pGky.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGky);
-            else return eval_pGky(a, b);
-        };
-        pG0z.eval = [&](point &a, point &b) {
-            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGkz);
-            else return eval_pGkz(a, b);
-        };
-        // f = partial phi/partial n
-        // g = phi
-        // Phi =  g   |  f
-        VectorXd f(N);
-        VectorXd gX(N), gY(N), gZ(N);
-        for (int i = 0; i < N; ++i) {
-            f(i) = phi(i + N) * weight[i];
-            gX(i) = phi(i) * normalX[i] * weight[i];
-            gY(i) = phi(i) * normalY[i] * weight[i];
-            gZ(i) = phi(i) * normalZ[i] * weight[i];
-        }
-
-
-        G0.initialize(np, source, target, f, N, M, maxPoint, maxLevel);
-        pG0x.initialize(np, source, target, gX, N, M, maxPoint, maxLevel);
-        pG0y.initialize(np, source, target, gY, N, M, maxPoint, maxLevel);
-        pG0z.initialize(np, source, target, gZ, N, M, maxPoint, maxLevel);
-
-        Gk.initialize(np, source, target, f, N, M, maxPoint, maxLevel);
-        pGkx.initialize(np, source, target, gX, N, M, maxPoint, maxLevel);
-        pGky.initialize(np, source, target, gY, N, M, maxPoint, maxLevel);
-        pGkz.initialize(np, source, target, gZ, N, M, maxPoint, maxLevel);
-
-
-        VectorXd retG0(M), retGk(M), retpG0X(M), retpG0Y(M), retpG0Z(M), retpGkX(M), retpGkY(M), retpGkZ(M);
-        G0.run(retG0);
-        Gk.run(retGk);
-        pG0x.run(retpG0X);
-        pG0y.run(retpG0Y);
-        pG0z.run(retpG0Z);
-        pGkx.run(retpGkX);
-        pGky.run(retpGkY);
-        pG0z.run(retpGkZ);
-
-        VectorXd ret(M);
-        ret = dE / dI * (retpGkX + retpGkY + retpG0Z) - (retpG0X + retpG0Y + retpG0Z) + retG0 - retGk;
-
-        return ret;
-    };
-
-
-    vector<point> rxnTarget;
-    rxnTarget.push_back(point(0., 0., 0.));
-
-    auto coarseRxn = [&](VectorXd &phi) {
-        return rxn(coarseSource, rxnTarget, coarseTriangle, coarseWeight, coarseNormalX, coarseNormalY,
-                   coarseNormalZ, phi);
-    };
-
-    std::cout << coarseRxn(start) << std::endl;
-    std::cout << coarseRxn(input) << std::endl;
+    std::cout << "fine error: " << (ret - input).norm() / input.norm() << std::endl;
+    std::cout << "extrapolation error: " << (2 * ret - start - input).norm() / input.norm() << std::endl;
 
 }
