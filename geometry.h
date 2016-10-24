55fdf18d6a83149b82360f67fa398d6ebf74f3ea
Yimin <zhongym8903@gmail.com>
2016-10-24 00:02:55 -0500

    minor change
    add example for single ion.

--- geometry.h
+++ geometry.h
@@ -45,6 +45,30 @@ inline double area(const double &a, const double &b, const double &c, const doub
            atan(sqrt(tan(0.5 * s) * tan(0.5 * (s - a)) * tan(0.5 * (s - b)) * tan(0.5 * (s - c))));
 }
 
+
+inline double theta(double psi_i, double psi_t, double phi) {
+    using std::cos;
+    using std::sqrt;
+    using std::sin;
+    double lambda = cos(psi_i) / cos(psi_t);
+    double sb = sin(phi / 2.0);
+
+    double a = sb * lambda;
+    a = 4 * a * a - 1.0;
+    double b = -4 * sb * sb * lambda;
+    double l = (-b - sqrt(b * b - 4 * a)) / 2.0 / a;
+
+    double _a = l * sin(psi_i);
+    double _b = sqrt(4 * sb * sb - cos(psi_t) * cos(psi_t)) * l * lambda;
+
+
+    double ret = (1 + _a * _a - _b * _b) / (2 * _a);
+    if (abs(ret - 1.0) < 1e-12) return 0.;
+    else {
+        return acos(ret);
+    }
+
+}
 /*
  * a single point quadrature rule for non-singular triangles
  */
@@ -57,11 +81,46 @@ inline void quadrature(vector<point> &source,
                        point &c,
                        int &triangleId) noexcept {
 
+    double alpha, beta, gamma, psi_t, T_s;
+    alpha = angle(b, c);
+    beta = angle(c, a);
+    gamma = angle(a, b);
+    T_s = triangleArea;
+
+    psi_t = asin(sqrt(cos(beta) * cos(beta) + cos(gamma) * cos(gamma) - 2 * cos(alpha) * cos(beta) * cos(gamma)) /
+                 sin(alpha));
+
+    double qqw[3] = {-sqrt(3.) / sqrt(5.), 0., sqrt(3.) / sqrt(5.)};
+    double www[3] = {5. / 18., 4. / 9., 5. / 18.};
+    int N = 3;
+    double theta_c = 0.;
+    double psi_c = 0.;
+
+    for (int i = 0; i < N; ++i) {
+        double psi_ = psi_t + (M_PI / 2.0 - psi_t) * (1.0 + qqw[i]) / 2.0;
+        double theta_l = theta(psi_, psi_t, beta);
+        double theta_u = alpha - theta(psi_, psi_t, gamma);
+        assert(theta_u > theta_l);
+        theta_c += (M_PI / 2.0 - psi_t) * (theta_u * theta_u - theta_l * theta_l) * sin(psi_) * www[i];
+        psi_c += (M_PI / 2.0 - psi_t) * (theta_u - theta_l) * psi_ * sin(psi_) * www[i];
+    }
+
+    theta_c /= 2.0 * T_s;
+    psi_c /= T_s;
+
+
+    double mu = (sin(theta_c) * sin(psi_c) -
+                 cos(psi_c) * sqrt(1 - cos(beta) * cos(beta) / sin(psi_t) / sin(psi_t)) / cos(psi_t)) / sin(alpha);
+    double lambda = cos(theta_c) * sin(psi_c) - cos(beta) * cos(psi_c) / cos(psi_t) - mu * cos(alpha);
+    double q = 1.0 / (lambda + mu + cos(psi_c) / cos(psi_t));
+    mu *= q;
+    lambda *= q;
+
     /*
      * barycentric coordinate of "barycentric" point
      */
-    double mu = 1.0 / 3.0;
-    double lambda = mu;
+//    double mu = 1.0 / 3.0;
+//    double lambda = mu;
 
     /*
      *  Cartesian coordinate
--- main.cpp
+++ main.cpp
@@ -18,21 +18,21 @@ int main() {
     /*
      * allocating source, target locations and weights
      */
-    vector<point> coarseSource, fineSource;
-    vector<point> coarseTarget, fineTarget;
-    vector<point> coarseTriangle, fineTriangle;
-    vector<double> coarseWeight, fineWeight;
+    vector<point> coarseSource;
+    vector<point> coarseTarget;
+    vector<point> coarseTriangle;
+    vector<double> coarseWeight;
 
     /*
      * cube radius and sphere radius
      */
     double cRadius = 2.0;
-    double sRadius = 1.5;
+    double sRadius = 1.0;
 
     /*
      * slice x slice grid on each face.
      */
-    int slice = 32;
+    int slice = 16;
 
     cubeProjection(coarseSource, coarseWeight, coarseTriangle, cRadius, sRadius, slice);
     coarseTarget = coarseSource;
@@ -47,23 +47,8 @@ int main() {
         coarseNormalZ[i] = coarseSource[i].z / sRadius;
     }
 
-
-    cubeProjection(fineSource, fineWeight, fineTriangle, cRadius, sRadius, 2 * slice);
-    fineTarget = fineSource;
-
-    vector<double> fineNormalX(fineSource.size());
-    vector<double> fineNormalY(fineSource.size());
-    vector<double> fineNormalZ(fineSource.size());
-
-    for (int i = 0; i < fineSource.size(); ++i) {
-        fineNormalX[i] = fineSource[i].x / sRadius;
-        fineNormalY[i] = fineSource[i].y / sRadius;
-        fineNormalZ[i] = fineSource[i].z / sRadius;
-    }
-
-
     int np = 6;
-    int maxPoint = 320;
+    int maxPoint = 160;
     int maxLevel = 10;
 
     double k = 0.03;
@@ -204,15 +189,10 @@ int main() {
 
 
     int N = (int) coarseSource.size();
-    int M = (int) fineSource.size();
+
     auto coarseMap = [&](VectorXd &phi) {
         return Mapping(coarseSource, coarseTarget, coarseTriangle, coarseWeight, coarseNormalX, coarseNormalY,
                        coarseNormalZ, phi);
-    };
-
-    auto fineMap = [&](VectorXd &phi) {
-        return Mapping(fineSource, fineTarget, fineTriangle, fineWeight, fineNormalX, fineNormalY,
-                       fineNormalZ, phi);
     };
 
     VectorXd input(2 * N);
@@ -232,38 +212,100 @@ int main() {
         output(i) = 1.0 / dI / 4.0 / M_PI / sRadius;
     }
 
-    gmres(coarseMap, output, start, 10, 40, 1e-7);
-
-
-    VectorXd input_(2 * M);
-    VectorXd start_(2 * M);
-
-    for (int i = 0; i < M; ++i) {
-        input_(i) = 1.0 / dE / 4.0 / M_PI / sRadius / (1 + k * sRadius);
-        input_(i + M) = -1.0 / dI / 4.0 / M_PI / sRadius / sRadius;
-    }
-
-    start_.setZero();
-
-    VectorXd output_(2 * M);
-    output_.setZero();
-    for (int i = 0; i < M; ++i) {
-        output_(i) = 1.0 / dI / 4.0 / M_PI / sRadius;
-    }
-
-    gmres(fineMap, output_, start_, 10, 40, 1e-7);
-
-
-    vector<int> proj;
-    projection(proj, coarseSource, fineSource);
-    VectorXd ret(2 * N);
-    for (int i = 0; i < N; ++i) {
-        ret(i) = start_(proj[i]);
-        ret(i + N) = start_(M + proj[i]);
-    }
+    gmres(coarseMap, output, start, 20, 40, 1e-7);
 
     std::cout << "coarse error :" << (start - input).norm() / input.norm() << std::endl;
-    std::cout << "fine error: " << (ret - input).norm() / input.norm() << std::endl;
-    std::cout << "extrapolation error: " << (2 * ret - start - input).norm() / input.norm() << std::endl;
+
+    auto rxn = [&](vector<point> &source, vector<point> &target, vector<point> triangle, vector<double> &weight,
+                   vector<double> &normalX, vector<double> &normalY, vector<double> &normalZ, VectorXd &phi) {
+        int N = (int) source.size();
+        int M = (int) target.size();
+
+        assert(phi.rows() == 2 * N);
+
+        kernel G0, Gk, pG0x, pG0y, pG0z, pGkx, pGky, pGkz;
+        G0.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_G0);
+            else return eval_G0(a, b);
+        };
+        Gk.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_Gk);
+            else return eval_Gk(a, b);
+        };
+        pG0x.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0x);
+            else return eval_pG0x(a, b);
+        };
+        pG0y.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0y);
+            else return eval_pG0y(a, b);
+        };
+        pG0z.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pG0z);
+            else return eval_pG0z(a, b);
+        };
+        pGkx.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGkx);
+            else return eval_pGkx(a, b);
+        };
+        pGky.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGky);
+            else return eval_pGky(a, b);
+        };
+        pG0z.eval = [&](point &a, point &b) {
+            if (a == b) return singularIntegral(a, triangle, sRadius, eval_pGkz);
+            else return eval_pGkz(a, b);
+        };
+        // f = partial phi/partial n
+        // g = phi
+        // Phi =  g   |  f
+        VectorXd f(N);
+        VectorXd gX(N), gY(N), gZ(N);
+        for (int i = 0; i < N; ++i) {
+            f(i) = phi(i + N) * weight[i];
+            gX(i) = phi(i) * normalX[i] * weight[i];
+            gY(i) = phi(i) * normalY[i] * weight[i];
+            gZ(i) = phi(i) * normalZ[i] * weight[i];
+        }
+
+
+        G0.initialize(np, source, target, f, N, M, maxPoint, maxLevel);
+        pG0x.initialize(np, source, target, gX, N, M, maxPoint, maxLevel);
+        pG0y.initialize(np, source, target, gY, N, M, maxPoint, maxLevel);
+        pG0z.initialize(np, source, target, gZ, N, M, maxPoint, maxLevel);
+
+        Gk.initialize(np, source, target, f, N, M, maxPoint, maxLevel);
+        pGkx.initialize(np, source, target, gX, N, M, maxPoint, maxLevel);
+        pGky.initialize(np, source, target, gY, N, M, maxPoint, maxLevel);
+        pGkz.initialize(np, source, target, gZ, N, M, maxPoint, maxLevel);
+
+
+        VectorXd retG0(M), retGk(M), retpG0X(M), retpG0Y(M), retpG0Z(M), retpGkX(M), retpGkY(M), retpGkZ(M);
+        G0.run(retG0);
+        Gk.run(retGk);
+        pG0x.run(retpG0X);
+        pG0y.run(retpG0Y);
+        pG0z.run(retpG0Z);
+        pGkx.run(retpGkX);
+        pGky.run(retpGkY);
+        pG0z.run(retpGkZ);
+
+        VectorXd ret(M);
+        ret = dE / dI * (retpGkX + retpGkY + retpG0Z) - (retpG0X + retpG0Y + retpG0Z) + retG0 - retGk;
+
+        return ret;
+    };
+
+
+    vector<point> rxnTarget;
+    rxnTarget.push_back(point(0., 0., 0.));
+
+    auto coarseRxn = [&](VectorXd &phi) {
+        return rxn(coarseSource, rxnTarget, coarseTriangle, coarseWeight, coarseNormalX, coarseNormalY,
+                   coarseNormalZ, phi);
+    };
+
+    std::cout << coarseRxn(start) << std::endl;
+    std::cout << coarseRxn(input) << std::endl;
 
 }
